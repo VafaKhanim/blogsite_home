3b86667f0e943a82da50cf93375b1cbd51ef57d9
VafaKhanim <vafakhanim.h@gmail.com>
2025-02-28 01:09:07 +0400

    updated vers

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, about_page
 urlpatterns = [
-    path('', home_view, name = 'home_view')
+    path('', home_view, name = 'home_view'),
+    path('about/', about_page, name = 'about_page')
 ]
--- core/views.py
+++ core/views.py
@@ -3,4 +3,26 @@ from .models import Post
 # Create your views here.
 def home_view(request):
     posts = Post.objects.all()
-    return render(request, 'home.html', {'posts' : posts})
+    context_home = {
+        'title' : 'Clean Blog',
+        'page_title': 'Clean Blog',
+        'page_subtitle': 'A Blog Theme by Start Bootstrap',
+        'back_image' : 'home-bg'
+    }
+
+    # posts və context_home birləşdirilir
+    context = {
+        'posts': posts,
+        **context_home,  # context_home ilə posts birləşdirilir
+    }
+
+    return render(request, 'home.html', context)
+
+def about_page(request):
+    context = {
+        'title': 'About Us',
+        'page_title': 'About Us',
+        'page_subtitle': 'Learn more about us',
+        'back_image' : 'about-bg'
+    }
+    return render(request, 'about.html', context)
